0c28566afd5a19ac478a5d5515052762f38d1d97
bariseryuz <fa-der-19981@hotmail.com>
2026-02-08 14:17:11 +0300

    feat: Always show link column in dashboard
    - Hardcoded link column to always appear first in table
    - Added link field to all source fieldSchemas
    - Link column now mandatory for all sources regardless of client customization
    - Ensures users always have access to source URLs

--- backend/add-sources.js
+++ backend/add-sources.js
@@ -8,6 +8,7 @@ const sources = [
     url: 'https://apps-secure.phoenix.gov/PDD/Search/IssuedPermit',
     aiPrompt: 'Fill out the permit search form: 1) Select "ALL PERMITS" for Permit Type, 2) Select "10 or More Family Units" for Structure Class, 3) Set Start date to 365 days ago from today, 4) Click Search, 5) Sort results by Structure Class, 6) Click "Create List" to show all results, 7) Extract permit data from all pages including permit number, address, contractor information, and project value',
     fieldSchema: {
+      link: { required: true },
       permit_number: { required: true },
       address: { required: true },
       contractor_name: { required: false },
@@ -22,6 +23,7 @@ const sources = [
     url: 'https://data.nashville.gov/datasets/2576bfb2d74f418b8ba8c4538e4f729f_0/explore?filters=eyJQZXJtaXRfVHlwZV9EZXNjcmlwdGlvbiI6WyJCdWlsZGluZyBSZXNpZGVudGlhbCAtIE5ldyJdLCJQZXJtaXRfU3VidHlwZV9EZXNjcmlwdGlvbiI6WyJNdWx0aWZhbWlseSwgVHJpLVBsZXgsIFF1YWQsIEFwYXJ0bWVudHMiLCJNdWx0aWZhbWlseSwgVG93bmhvbWUiXSwiRGF0ZV9FbnRlcmVkIjpbMTUwMTY1MDAwMDAwMCwxNzY0ODI4MDAwMDAwXSwiRGF0ZV9Jc3N1ZWQiOlsxNjcwMjIwMDAwMDAwLDE3NjUwODcyMDAwMDBdLCJDb25zdF9Db3N0IjpbMCwyMjYwNjg3NTldfQ%3D%3D&location=36.213201%2C-86.071734%2C8.14&showTable=true',
     aiPrompt: 'The filters are already applied in the URL. Click to open or show the data table if needed, then extract all multi-family building permit information from all pages including permit numbers, addresses, contractor details, construction costs, and dates',
     fieldSchema: {
+      link: { required: true },
       permit_number: { required: true },
       address: { required: true },
       contractor_name: { required: false },
@@ -35,6 +37,7 @@ const sources = [
     url: 'https://eservices.scottsdaleaz.gov/bldgresources/BuildingPermit/reports#',
     aiPrompt: 'Find and fill out the building permit search form with: Issue Date from 01/01/2024 to today, Permit Type: All, Structure Type: Multi-Family or Apartments (10+ units). Click Search or Submit. Then extract all permit data from the results including permit numbers, addresses, contractor information, and project values from all pages',
     fieldSchema: {
+      link: { required: true },
       permit_number: { required: true },
       address: { required: true },
       contractor_name: { required: false },
@@ -48,6 +51,7 @@ const sources = [
     url: 'https://data.mesaaz.gov/Development-Services/Building-Permits-RETIRED-/2gkz-7z4f/data_preview',
     aiPrompt: 'Apply filters to the data table: 1) Set Issue Date filter to 01/01/2024 or later, 2) Sort by Unit # column to filter out non-multifamily projects, 3) Extract all building permit data from the filtered results including permit numbers, addresses, contractor details, unit counts, and values from all pages',
     fieldSchema: {
+      link: { required: true },
       permit_number: { required: true },
       address: { required: true },
       contractor_name: { required: false },
@@ -61,6 +65,7 @@ const sources = [
     url: 'https://gis-mdc.opendata.arcgis.com/datasets/MDC::building-permits-issued-by-miami-dade-county-2-previous-years-to-present/explore?filters=eyJBcHBsaWNhdGlvbkRhdGUiOlsxNjQyMTg1NDkxOTc5LjIzLDE3NjUwODM2MDAwMDBdLCJBcHBsaWNhdGlvblR5cGVDb2RlIjpbMSw0MF0sIlBlcm1pdFR5cGUiOlsiQkxERyJdLCJBcHBsaWNhdGlvblR5cGVEZXNjcmlwdGlvbiI6WyJORVciXSwiUHJvcG9zZWRVc2VDb2RlIjpbMjIwLDQwMDNdLCJQcm9wb3NlZFVzZURlc2NyaXB0aW9uIjpbIjUgVU5JVFMgT1IgTU9SRSAgLSBSRVNJREVOVElBTCJdLCJDb250cmFjdG9yQ2l0eSI6WyJNSUFNSSJdfQ%3D%3D',
     aiPrompt: 'Filters are pre-applied for multi-family residential permits (5+ units) in Miami. Open or show the data table, then extract all building permit information from all pages including permit numbers, addresses, contractor details, proposed use, application dates, and project details',
     fieldSchema: {
+      link: { required: true },
       permit_number: { required: true },
       address: { required: true },
       contractor_name: { required: false },
@@ -74,6 +79,7 @@ const sources = [
     url: 'https://aca-prod.accela.com/FTL/Cap/GlobalSearchResults.aspx?isNewQuery=yes&QueryText=new%20construction%20Multi',
     aiPrompt: 'The search is already performed for "new construction Multi". Extract all project data from the results table including record numbers, addresses, project names, contractor information, status, and dates from all pages',
     fieldSchema: {
+      link: { required: true },
       permit_number: { required: true },
       address: { required: true },
       project_name: { required: false },
@@ -87,6 +93,7 @@ const sources = [
     url: 'https://experience.arcgis.com/experience/25641c3e2dab4eff803ed286bef11bf8/#data_s=id%3AdataSource_1-ConstructionInspections_863%3A193',
     aiPrompt: 'This is an ArcGIS Experience with construction inspection data. Find and open the data table view, then extract all construction permit and inspection records including permit numbers, addresses, contractor details, project types, and inspection dates from all available pages',
     fieldSchema: {
+      link: { required: true },
       permit_number: { required: true },
       address: { required: true },
       contractor_name: { required: false },
