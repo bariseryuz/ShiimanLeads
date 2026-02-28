0c9ea9b569fd5fbfd6054e73c171efdf72512a4c
bariseryuz <fa-der-19981@hotmail.com>
2026-02-28 18:58:46 +0300

    fix: Remove unused Lead model import from summarize route

--- backend/routes/summarize.js
+++ backend/routes/summarize.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const Alsummarize = require('../services/ai/Alsummarize');
-const { Lead } = require('../models');
 const logger = require('../utils/logger');
 
 /**
